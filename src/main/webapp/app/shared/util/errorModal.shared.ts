cb45fa0fd93ca1b0aec91a508eb474c22187979a
Mounir Mohammed <devmounir@gmail.Com>
2023-05-25 01:43:33 +0200

    FIX double alert

--- src/main/webapp/app/shared/util/errorModal.shared.ts
+++ src/main/webapp/app/shared/util/errorModal.shared.ts
@@ -8,32 +8,22 @@ export class ErrorModalUtil {
   constructor(protected alertService: AlertService) {}
 
   throwAlertErrorLoadingModel(id: number) {
-    this.alertService.get().push(
-      this.alertService.addAlert(
-        {
-          type: 'danger',
-          message: 'Error on loading printing model for Accreditation {{ id }}, Please contact administrator',
-          translationKey: 'sigmaEventsApp.accreditation.print.modelError',
-          translationParams: { id: id },
-          timeout: 1000,
-        },
-        this.alertService.get()
-      )
-    );
+    this.alertService.addAlert({
+      type: 'danger',
+      message: 'Error on loading printing model for Accreditation {{ id }}, Please contact administrator',
+      translationKey: 'sigmaEventsApp.accreditation.print.modelError',
+      translationParams: { id: id },
+      timeout: 4000,
+    });
   }
 
   throwAlertErrorUnauthorizedPrinting(id: number) {
-    this.alertService.get().push(
-      this.alertService.addAlert(
-        {
-          type: 'danger',
-          message: 'Unauthorized Printing Accreditation {{ id }}, Please contact administrator',
-          translationKey: 'sigmaEventsApp.accreditation.print.unauthorizedPrintingError',
-          translationParams: { id: id },
-          timeout: 1000,
-        },
-        this.alertService.get()
-      )
-    );
+    this.alertService.addAlert({
+      type: 'danger',
+      message: `Unauthorized Printing Accreditation ${id}, Please contact administrator`,
+      translationKey: 'sigmaEventsApp.accreditation.print.unauthorizedPrintingError',
+      translationParams: { id: id },
+      timeout: 4000,
+    });
   }
 }
