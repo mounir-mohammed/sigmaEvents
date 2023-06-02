6a826e4e1a2ecb9e64e146d38923560fcf2cda84
Mounir Mohammed <devmounir@gmail.Com>
2023-06-02 18:57:41 +0200

    add FeaturePolicyFilter

---
+++ src/main/java/ma/sig/events/config/FeaturePolicyFilter.java
@@ -0,0 +1,26 @@
+package ma.sig.events.config;
+
+import java.io.IOException;
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class FeaturePolicyFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+        httpServletResponse.setHeader(
+            "Feature-Policy",
+            "accelerometer 'none'; camera *; geolocation 'none'; " +
+            "gyroscope 'none'; magnetometer 'none'; microphone 'none'; payment *; usb 'none'"
+        );
+        chain.doFilter(request, response);
+    }
+    // Implement the other methods of the Filter interface (init and destroy) if necessary
+
+}
