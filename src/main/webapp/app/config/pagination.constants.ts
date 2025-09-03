f4b15c79ba65910a660de47ad796840b44e24eba
Mounir Mohammed <devmounir@gmail.Com>
2025-09-03 18:53:13 +0200

    Revert "Change RECORD_ITEMS to 1000 size"
    This reverts commit c34a899c

--- src/main/webapp/app/config/pagination.constants.ts
+++ src/main/webapp/app/config/pagination.constants.ts
@@ -1,5 +1,5 @@
 export const TOTAL_COUNT_RESPONSE_HEADER = 'X-Total-Count';
 export const PAGE_HEADER = 'page';
 export const ITEMS_PER_PAGE = 20;
-export const RECORD_ITEMS = 999;
+export const RECORD_ITEMS = 200;
 export const CACHE_RECORD_ITEMS = 1000;
